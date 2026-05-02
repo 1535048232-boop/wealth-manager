3c36b6c2db11e1a5aa8eb825c8af43bb5dcd583b
1535048232-boop <1535048232@qq.com>
2026-05-02 18:00:36 +0800

    Fix chart clipping and family creation flow

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const exclusionList = require("metro-config/private/defaults/exclusionList").default;
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.blockList = exclusionList([
+  new RegExp(`${path.resolve(__dirname, ".env.release.local").replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}$`),
+  new RegExp(`${path.resolve(__dirname, ".env.release.local.example").replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}$`),
+]);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
